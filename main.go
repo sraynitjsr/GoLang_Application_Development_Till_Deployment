d1714fa053c7bee1657bd98fb808d640aeef6777
unknown <subhradeepray2017@gmail.com>
2023-04-28 23:38:14 +0530

    Creating a working service

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func doTask(ch chan string, str string) {
+	for {
+		ch <- str
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Inside Running Service Module")
+	myChannel := make(chan string)
+	go doTask(myChannel, "Hello World")
+	for data := range myChannel {
+		fmt.Println("Current Data From Channel is =>", data)
+		time.Sleep(time.Second * 1)
+	}
+	interruptChannel := make(chan os.Signal, 1)
+	signal.Notify(interruptChannel)
+	s := <-interruptChannel
+	fmt.Println("Got Signal =>", s)
 }
